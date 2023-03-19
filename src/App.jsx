53631cff680ce2cc1b30bacd2d970b96684b1cf7
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-19 10:58:50 +0200

    feat: add tailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl text-red-500 font-bold underline text-center ">
+     React Intermediate Project with Redux
+    </h1>
+  )
+}
