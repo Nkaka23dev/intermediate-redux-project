063139f1dcaa4cabb52807db368e313e1bcbc041
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-09 17:38:32 +0200

    feat: added search functionality

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='py-10'>
-      <h1 className='text-center text-xl tracking-wide'>All Right Reserved &copy; 2023 The Gym Rwanda, Eric Nkaka</h1>
+    <div className='py-10 px-5 md:py-5'>
+      <h1 className='text-center md:text-sm text-xl tracking-wide'>All Right Reserved &copy; 2023 The Gym Rwanda, Eric Nkaka</h1>
     </div>
   )
 }
